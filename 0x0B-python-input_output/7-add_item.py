f930bdfe13845947fa2d9fd6d618ac2a5bb85be5
Alabere-coder <alabere4real247@gmail.com>
2023-01-10 16:39:55 +0000

    task 7 correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-from os import path
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""Module: 7-add_item
+This is a script that adds all arguments to a Python list.
+Save them to a file.
+"""
 
-if path.exists('add_item.json'):
-    obj_json_file = load_from_json_file('add_item.json')
-else:
-    obj_json_file = []
 
-for i in range(1, len(argv)):
-    obj_json_file.append(argv[i])
+import json
+import sys
+import os.path
 
-save_to_json_file(obj_json_file, 'add_item.json')
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+new_file = "add_item.json"
+new_list = []
+
+if os.path.exists(new_file) and os.path.getsize(new_file) > 0:
+    new_list = load_from_json_file(new_file)
+
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        new_list.append(elem)
+
+save_to_json_file(new_list, new_file)
