60d885f10cf06347852bb705b9a793c05c9b9438
Alabere-coder <alabere4real247@gmail.com>
2023-03-31 08:04:33 -0700

    What is my status?

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Check status"""
+"""Fetches the URL: https://intranet.hbtn.io/status
+with `requests` module
+"""
+
 import requests
 
 
-def status():
-    """status"""
-    result = requests.get("https://intranet.hbtn.io/status")
-
-    print("Body response:")
-    print("\t- type: {}".format(type(result.text)))
-    print("\t- content: {}".format(result.text))
-
 if __name__ == "__main__":
-    status()
+    req = requests.get('https://intranet.hbtn.io/status')
+
+    print('Body response:')
+    print('\t- type: {_type}'.format(_type=type(req.text)))
+    print('\t- content: {_content}'.format(_content=req.text))
